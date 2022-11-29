10bbcece6b550e90db3c3c49bad958ec76a9e524
Elif Ercan <ercanelif450@gmail.com>
2022-11-30 01:06:58 +0300

    first commit

---
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Helpers;
+using MyAspNetCoreApp.Web.Models;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IHelper _helper;
+        private AppDbContext _context;  
+        private readonly ProductRepository _productRepository;
+        public ProductsController(AppDbContext context,IHelper helper)
+        {
+            _productRepository =new  ProductRepository();
+            _helper = helper;   
+            _context = context;
+
+            
+
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            TempData["status"] = "Ürün başarıyla eklendi.";
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var product=_context.Products.Find(id);
+
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Update(Product updateProduct)
+        {
+            _context.Products.Update(updateProduct);
+            _context.SaveChanges();
+            TempData["status"] = "Ürün başarıyla güncellendi.";
+            return RedirectToAction("Index");
+        }
+    }
+}
